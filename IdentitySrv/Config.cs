089b88762e3a1c14ea428dcb50cdbae1b218e67a
Tareq Alkatout <tareqalkatout@hotmail.com>
2019-06-28 20:24:15 +0400

    add identityserver4 and configure it.

---
+++ IdentitySrv/Config.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySrv
+{
+    public class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> { new TestUser
+            {
+                 SubjectId = "1" ,
+                 Username = "tariq" ,
+                 Password = "tariq"
+            } };
+        }
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("valuesApi" , "Our Api that will be documented by swashbuckle")
+            };
+        }
+
+        public static IEnumerable<Client> GetAllClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                     ClientId = "swaggerapiui",
+                     ClientName = "Swagger API UI",
+                     AllowedGrantTypes = GrantTypes.Implicit ,
+                     RedirectUris = {"http://localhost:5001/swagger/oauth2-redirect.html" },
+                     PostLogoutRedirectUris = { "http://localhost:5001/swagger" },
+                     AllowedScopes = { "valuesApi" },
+                     AllowAccessTokensViaBrowser = true
+                }
+            };
+        }
+    }
+}
